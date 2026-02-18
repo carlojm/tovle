bedbb67c2bce7c62ba39f313280585296adb89f8
Carlo <CarloMehegan@gmail.com>
2026-02-17 20:10:30 -0500

    full tov coord set

---
+++ tovle/tovparser.py
@@ -0,0 +1,220 @@
+# this is straight chatgpt. thank you chatgpt.
+
+waypoints_text = """
+waypoint:1:1:-701:74:1148:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:2:2:-593:63:969:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:3:3:-409:74:670:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:4:4:-555:63:642:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:5:5:-675:15:629:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:6:6:-751:53:502:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:7:7:-744:34:390:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:8:8:-633:9:409:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:9:9:-437:5:410:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:10:10:-269:49:296:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:11:11:-187:61:216:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:12:12:-321:27:147:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:13:13:-364:7:-55:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:14:14:-175:57:-73:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:15:15:-126:61:-181:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:16:16:45:62:-243:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:17:17:180:71:-165:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:18:18:322:73:-273:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:19:19:269:73:-389:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:20:20:257:53:-411:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:21:21:73:32:-441:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:22:22:-46:62:-410:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:23:23:-185:40:-371:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:24:24:-327:39:-397:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:25:25:-327:49:-534:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:26:26:-360:53:-593:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:27:27:-192:34:-535:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:28:28:10:42:-637:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:29:29:237:43:-620:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:30:30:433:20:-529:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:31:31:624:28:-616:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:32:32:723:58:-570:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:33:33:852:29:-642:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:34:34:770:10:-528:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:35:35:673:38:-371:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:36:36:794:45:-212:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:37:37:801:34:-81:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:38:38:792:43:214:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:39:39:720:46:268:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:40:40:669:59:368:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:41:41:824:63:376:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:42:42:718:56:542:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:43:43:570:16:572:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:44:44:515:52:530:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:45:45:703:27:682:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:46:46:725:46:748:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:47:47:823:41:706:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:48:48:816:48:932:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:49:49:590:65:882:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:50:50:598:42:1132:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:51:51:683:49:1195:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:52:52:516:72:1204:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:53:53:431:7:1193:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:54:54:244:58:1185:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:55:55:-31:50:1214:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:56:56:-101:25:1294:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:57:57:80:59:1399:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:58:58:271:13:1499:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:59:59:272:61:1560:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:60:60:353:56:1501:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:61:61:457:67:1440:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:62:62:765:43:1318:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:63:63:820:58:1395:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:64:64:750:74:1554:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:65:65:732:49:1814:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:66:66:773:51:1828:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:67:67:856:55:1897:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:68:68:550:43:1809:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:69:69:364:19:1812:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:70:70:188:39:1862:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:71:71:111:69:1728:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:72:72:20:76:1646:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:73:73:2:49:1754:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:74:74:-106:36:1836:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:75:75:-236:40:1793:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:76:76:-270:27:1685:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:77:77:-340:14:1858:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:78:78:-573:24:1835:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:79:79:-755:56:1742:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:80:80:-801:64:1855:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:81:81:-897:68:1787:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:82:82:-1073:64:1798:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:83:83:-1197:67:1855:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:84:84:-1255:43:1669:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:85:85:-1382:6:1662:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:86:86:-1448:38:1794:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:87:87:-1574:38:1822:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:88:88:-1523:19:1683:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:89:89:-1742:66:1648:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:90:90:-1969:47:1647:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:91:91:-1926:43:1776:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:92:92:-2133:51:1790:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:93:93:-2198:61:1882:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:94:94:-2162:60:1484:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:95:95:-2168:69:1317:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:96:96:-2191:65:1162:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:97:97:-2151:51:1061:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:98:98:-1885:60:1034:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:99:99:-1897:43:961:10:false:0:Treasures of Viridia:false:0:0:false
+waypoint:100:0:-1766:80:856:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:101:1:-1873:49:692:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:102:2:-2165:52:783:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:103:3:-2180:57:554:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:104:4:-2129:40:443:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:105:5:-2031:74:449:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:106:6:-2043:37:132:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:107:7:-2189:49:231:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:108:8:-2190:65:30:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:109:9:-2097:37:-143:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:110:10:-1954:36:-280:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:111:11:-1883:69:-330:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:112:12:-1796:42:-488:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:113:13:-1940:32:-542:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:114:14:-2010:28:-602:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:115:15:-2162:35:-614:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:116:16:-1840:47:-623:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:117:17:-1753:48:-627:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:118:18:-1606:31:-355:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:119:19:-1599:61:-543:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:120:20:-1432:20:-485:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:121:21:-1364:38:-481:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:122:22:-1215:17:-510:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:123:23:-1056:49:-453:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:124:24:-964:44:-566:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:125:25:-735:16:-531:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:126:26:-770:42:-381:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:127:27:-605:14:-182:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:128:28:-546:41:-234:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:129:29:-412:45:-344:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:130:30:-522:49:0:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:131:31:-579:36:210:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:132:32:-827:63:119:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:133:33:-895:35:54:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:134:34:-710:41:0:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:135:35:-813:32:-104:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:136:36:-898:9:-134:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:137:37:-983:64:-193:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:138:38:-993:29:-321:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:139:39:-1268:58:-382:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:141:41:-1027:69:-77:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:140:40:-1197:65:-89:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:142:42:-1021:62:198:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:143:43:-915:66:280:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:144:44:-939:63:454:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:145:45:-869:41:707:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:146:46:-888:57:826:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:147:47:-936:53:1014:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:148:48:-1038:73:1028:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:149:49:-1037:35:1187:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:150:50:-1147:40:1143:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:151:51:-1405:52:1103:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:152:52:-1525:7:1244:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:153:53:-1757:63:1156:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:154:54:-1966:49:1198:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:155:55:-1851:80:1387:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:156:56:-2003:64:1487:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:157:57:-1773:42:1498:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:158:58:-1724:29:1530:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:159:59:-1563:9:1473:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:160:60:-1064:43:1476:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:161:61:-973:56:1672:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:162:62:-509:45:1629:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:163:63:-482:62:1365:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:164:64:-225:44:1432:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:165:65:-260:54:1091:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:166:66:-263:43:847:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:167:67:-333:61:868:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:168:68:-317:46:771:12:false:0:Treasures of Viridia:false:0:0:false
+waypoint:169:69:-110:70:684:12:false:0:Treasures of Viridia:false:0:0:false
+"""
+# PASTE ALL 169 WAYPOINTS HERE (replace the above lines)
+
+import json
+
+def parse_waypoints(text):
+    lines = text.strip().split('\n')
+    waypoints = []
+    
+    for line in lines:
+        if not line.strip():  # Skip empty lines
+            continue
+            
+        parts = line.split(':')
+        
+        # Extract: waypoint:id:id:x:y:z:...
+        waypoint_id = int(parts[1])
+        x = int(parts[3])
+        z = int(parts[5])  # Skip y (parts[4]), use z
+        
+        waypoints.append({
+            "id": waypoint_id,
+            "coordinates": {
+                "x": x,
+                "z": z
+            }
+        })
+    
+    return waypoints
+
+# Parse the waypoints
+data = parse_waypoints(waypoints_text)
+
+# Write to JSON file
+with open('tovs.json', 'w') as f:
+    json.dump(data, f, indent=2)
+
+print(f"✅ Generated tovs.json with {len(data)} waypoints!")
+
+# Also print the coordinate bounds for your mapBounds
+x_coords = [w['coordinates']['x'] for w in data]
+z_coords = [w['coordinates']['z'] for w in data]
+
+print(f"\n📍 Coordinate Bounds:")
+print(f"X: {min(x_coords)} to {max(x_coords)}")
+print(f"Z: {min(z_coords)} to {max(z_coords)}")
+print(f"\nSuggested mapBounds:")
+print(f"{{ minX: {min(x_coords)}, maxX: {max(x_coords)}, minZ: {min(z_coords)}, maxZ: {max(z_coords)} }}")
