cf446ee4e0482e75cc3523aa080f4a637f40a935
Carlo <CarloMehegan@gmail.com>
2026-03-11 18:15:31 -0400

    backend serves a daily cache

--- server/index.js
+++ server/index.js
@@ -1,33 +1,48 @@
 require('dotenv').config()
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const app = express()
-const tovs = require('./data/tovs.json')
-// app.use(cors())
+
+const schedule = require('./data/schedule.json')
+
+app.use(cors())
 app.use(express.json())
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 
+const getEasternDateString = () => {
+  return new Date().toLocaleDateString('en-CA', { timeZone: 'America/New_York' })
+  // en-CA locale gives YYYY-MM-DD format
+}
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+const getDailyFallback = (dateString, availableIds) => {
+  // turn the date string into a number
+  const seed = dateString.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)
+  // return availableIds[seed % availableIds.length]
+  return seed % 14 + 1
+}
+
+const getScheduledId = (dateString) => {
+  const entry = schedule.find(item => item.date === dateString)
+  if (!entry) return null
+  if (entry.id === 'random') return null
+  return entry.id
+}
+
+app.get('/api/daily', (request, response) => {
+  const today = getEasternDateString() // ex. "2026-03-01"
+  const availableIds = schedule.map(t => t.id)
+
+  const id = getScheduledId(today) ?? getDailyFallback(today, availableIds)
+
+  if (!id) {
+    return response.status(404).json({error: `No cache found for id ${id}`})
+  }
+  response.json(id)
 })
 
-app.get('/tovs', (request, response) => {
-  response.json(tovs)
-})
 
-app.get('/tovs/random', (request, response) => {
-  const random = tovs[Math.floor(Math.random() * tovs.length)]
-  response.json(random)
-})
-
-app.get('/tovs/daily', (request, response) => {
-  const daily = tovs[0]
-  response.json(daily)
-})
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
