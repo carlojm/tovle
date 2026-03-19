37d9ffdbb7a9828b2372bffd3ebad25293b3c6c7
Carlo <CarloMehegan@gmail.com>
2026-03-19 00:54:24 -0400

    first deploy!

--- server/index.js
+++ server/index.js
@@ -120,6 +120,6 @@ app.get('/api/daily', (request, response) => {
   response.json({ date: today, caches })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
