f597cf01fdd0176bd2ee1bda64f248d10a4bb0ca
Carlo <CarloMehegan@gmail.com>
2026-03-20 18:20:09 -0400

    info page

--- tovle/src/components/Info.jsx
+++ tovle/src/components/Info.jsx
@@ -1,3 +1,7 @@
+import './Info.css'
+import {Ship, DraftingCompass, Gift} from 'lucide-react'
+
+
 const Info = () => {
 	const today = new Date();
 	const year = today.getFullYear();
@@ -6,18 +10,59 @@ const Info = () => {
 	const formattedDate = `${month}/${date}/${year}`;
 
 	return (
-		<div style={{
-			display:'flex',
-			flexDirection: 'column',
-			alignItems: 'center'
-		}}>
-			<p><strong>Welcome to Tovle! WIP</strong></p>
-			{/* <p style={{fontStyle: 'italic'}}>Today's caches: {formattedDate}</p> */}
-			<p>{formattedDate}</p>
+		<div className="info-box">
+			<h1>Welcome to Tovle!</h1>
+        <p>
+          I am building this project to learn React and CSS. 
+          Tovle is a fanmade site and not affiliated with Monumenta.
+        </p>
+        <p>For feedback or suggestions, message me @carlojm on Discord. Send in your own custom caches if you'd like!</p>
+        <p>
+          IMPORTANT! If you're reading this, you are playing pre-release.
+          Expect user data to get randomly erased or corrupted.
+          Actually if it gets corrupted that's weird and you should tell me.
+        </p>
 
-            <p>WIP</p>
-            <p>I am building Tovle as a way to learn React and CSS!</p>
-            <p>For feedback or suggestions message me @carlojm on Discord.</p>
+
+			<h1>Info</h1>
+
+        <div className="info-row">
+          <Ship size={20} color="var(--color-text)" />
+          <p>
+            Four new caches wash in with the tide every day at midnight EDT, UTC-04.
+            Pinpoint each cache's coordinates based on the image. Each daily set typically consists of
+            three standard caches and one custom cache unique to the day.
+          </p>
+        </div>
+
+        <div className="info-row">
+          <DraftingCompass size={20} color="var(--color-text)" />
+          <p>
+            Each guess will display info guiding you closer to the cache. Guess within 50 blocks to collect the cache.
+          </p>
+        </div>
+
+        <div className="info-row">
+          <Gift size={20} color="var(--color-text)" />
+          <p>
+            Open your caches in the Caches tab to obtain items at random.
+            Caches found in less guesses may contain better loot!
+            Use these items for various upgrades and bragging rights.
+          </p>
+        </div>
+
+        <h1>Login</h1>
+        <p>
+          Erasing your browser cache will erase your player data. To prevent losing data,
+          consider linking a Google account to your uid.
+
+        </p>
+        <p>
+          Important: This will link the associated email address to your uid in the app's Firestore database.
+          Consider using an email address that does not reveal personal info so I'm not up in your business like that.
+        </p>
+        <p style={{textAlign:"center"}}>(Login button here)</p>
+
 		</div>
 	);
 }
