db0baa28b55f81069989b0f0b0e3a69fb75d3321
Carlo <CarloMehegan@gmail.com>
2026-03-23 14:59:41 -0400

    change fishing net craft again

--- tovle/src/components/Crafting.jsx
+++ tovle/src/components/Crafting.jsx
@@ -42,13 +42,13 @@ const UPGRADES = [
     name: 'Fishing Net',
     description: [
       'None',
-      'Something to do while you swim from cache to cache. Fish will now show up in cache loot.',
+      'Buy a fishing net from Vigora. Fish will now show up in cache loot.',
       'Upgrade your net to catch more types of fish.',
       'None',
     ],
     maxTier: 2,
     costs: [
-      [{ itemId: 'celsian_fragment', quantity: 1 }, { itemId: 'hypercrystalline_shard', quantity: 1 }],
+      [{ itemId: 'den_piece_100', quantity: 1 }, { itemId: 'hypercrystalline_shard', quantity: 1 }],
       [{ itemId: 'twisted_strand', quantity: 2 }, { itemId: 'hypercrystalline_shard', quantity: 1 }],
     ]
   },
